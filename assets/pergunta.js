8044f6b2df5684b6e8092aa99ea858cb39a80cf1
ZWaretsckG <141160234+ZWaretsckG@users.noreply.github.com>
2024-10-24 11:30:33 -0300

    .

--- assets/pergunta.js
+++ assets/pergunta.js
@@ -1,52 +1,35 @@
 criaCartao(
-   'programaçao'  
-     'O que e html'
-    'E uma imagem de marcaçao de hipertexto'
-
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
+    )
+criaCartao(
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
 )
 criaCartao(
-    'programaçao'  
-      'O que e html'
-     'E uma imagem de marcaçao de hipertexto'
- 
-     criaCartao(
-        'programaçao'  
-          'O que e html'
-         'E uma imagem de marcaçao de hipertexto'
-      
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
+)
 
-         criaCartao(
-            'programaçao'  
-              'O que e html'
-             'E uma imagem de marcaçao de hipertexto'
-         
+criaCartao(
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
+)
 
-
-             criaCartao(
-                'programaçao'  
-                  'O que e html'
-                 'E uma imagem de marcaçao de hipertexto'
-             
-
-
-                 criaCartao(
-                    'programaçao'  
-                      'O que e html'
-                     'E uma imagem de marcaçao de hipertexto'
-                 
-
-
-
-
-
-
-
-
-
-
-
-
-
+criaCartao(
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
+)
+criaCartao(
+    'programaçao' , 
+     'O que e html',
+    'E uma imagem de marcaçao de hipertexto',
+)
 
 
 
