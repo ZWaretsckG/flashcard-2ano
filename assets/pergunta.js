5f4c74a709fd8c369d3d781218faf9f016c11645
ZWaretsckG <141160234+ZWaretsckG@users.noreply.github.com>
2024-10-24 11:14:46 -0300

    .

--- assets/pergunta.js
+++ assets/pergunta.js
@@ -4,3 +4,52 @@ criaCartao(
     'E uma imagem de marcaçao de hipertexto'
 
 )
+criaCartao(
+    'programaçao'  
+      'O que e html'
+     'E uma imagem de marcaçao de hipertexto'
+ 
+     criaCartao(
+        'programaçao'  
+          'O que e html'
+         'E uma imagem de marcaçao de hipertexto'
+      
+
+         criaCartao(
+            'programaçao'  
+              'O que e html'
+             'E uma imagem de marcaçao de hipertexto'
+         
+
+
+             criaCartao(
+                'programaçao'  
+                  'O que e html'
+                 'E uma imagem de marcaçao de hipertexto'
+             
+
+
+                 criaCartao(
+                    'programaçao'  
+                      'O que e html'
+                     'E uma imagem de marcaçao de hipertexto'
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
