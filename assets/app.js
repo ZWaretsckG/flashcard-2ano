6b0e3ec39d48e33116721067f784ba87b33304de
ZWaretsckG <141160234+ZWaretsckG@users.noreply.github.com>
2024-11-07 12:01:22 -0300

    .

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,44 @@
 function criarCartao(catgoria,pegunta,resposta)
       console.log(categoria,pergunta,reposta);
+    let container= document.createElement ('article')
+    cartao.className = 'cartao'
+    cartao.innerHTML
+   <div class =' cartao__conteudo'>
+   < div class name {
+   <div class='cartao__conteudo__pergunta'>
+   </div class='cartao__conteudo__resposta'>
+     </p> 
+   </div>
+   </div>
 
 
-    
+
+
+
+
+
+   </div>
+            
+      }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
